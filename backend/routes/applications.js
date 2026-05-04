db47e8365b71fe2c831b10cffab05990d7757ca9
Peter Lemreng <peterlemreng1987@gmail.com>
2026-05-04 13:12:46 +0300

    Protect application detail route with requireAdmin

--- backend/routes/applications.js
+++ backend/routes/applications.js
@@ -50,7 +50,7 @@ router.get("/", requireAdmin, async (req, res) => {
   }
 });
 
-router.get("/:id", async (req, res) => {
+router.get("/:id", requireAdmin, async (req, res) => {
   try {
     const { id } = req.params;
 
