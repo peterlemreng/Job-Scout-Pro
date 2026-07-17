1ea54bf7615031e1d88ec10d9c17b7405ac7e8ce
Peter Lemreng <peterlemreng1987@gmail.com>
2026-07-17 20:47:58 +0300

    Add job categories and Kenya counties data

---
+++ backend/data/categories.js
@@ -0,0 +1,23 @@
+module.exports = [
+  "Government",
+  "County Government",
+  "NGO",
+  "Health",
+  "Education",
+  "Engineering",
+  "ICT",
+  "Finance",
+  "Banking",
+  "Legal",
+  "Agriculture",
+  "Hospitality",
+  "Security",
+  "Construction",
+  "Internship",
+  "Attachment",
+  "Graduate Trainee",
+  "Remote Jobs",
+  "Part-time",
+  "Full-time"
+];
+
